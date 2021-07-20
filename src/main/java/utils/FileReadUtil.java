05cc83147eb5fc913fb7124d8b640122a42f3980
LIULONGTAO <nwpullt@163.com>
2021-07-20 10:53:09 +0800

    [20210720]-liulongtao FileReadUtil添加文件读取函数

--- src/main/java/utils/FileReadUtil.java
+++ src/main/java/utils/FileReadUtil.java
@@ -2,7 +2,10 @@ package utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * @author: LLT
@@ -11,6 +14,12 @@ import java.io.File;
  * @modified By
  */
 public class FileReadUtil {
+    /**
+     * 判断文件路径是否正确
+     *
+     * @param uri
+     * @return
+     */
     public static boolean checkUri(String uri) {
         //判断传入的uri路径是否合法
         if (StringUtils.isBlank(uri)) {
@@ -22,5 +31,26 @@ public class FileReadUtil {
             return false;
         }
         return true;//返回文件大小
+    }
+
+    /**
+     * 读取文件内容
+     *
+     * @param uri
+     * @return
+     */
+    public static String readFileContent(String uri) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("runoob.txt"));
+            StringBuffer content = new StringBuffer();
+            while (in.ready()) {
+                content.append(in.readLine());
+            }
+            in.close();
+            return content.toString();
+        } catch (IOException e) {
+            System.out.println("文件读取异常");
+        }
+        return null;
     }
 }
