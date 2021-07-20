8dabe21d15341f6793a33ad1720e003b38311979
LIULONGTAO <nwpullt@163.com>
2021-07-20 15:04:03 +0800

    [20210720]-liulongtao CodeStatisticResponse实体类toString函数修改

--- src/main/java/vo/CodeStatisticResponse.java
+++ src/main/java/vo/CodeStatisticResponse.java
@@ -28,6 +28,12 @@ public class CodeStatisticResponse {
     private int noteLines;
 
     /**
+     * 空白行数
+     */
+    private int blackLines;
+
+
+    /**
      * 待搜索字符
      */
     private String key;
@@ -35,8 +41,14 @@ public class CodeStatisticResponse {
      * 字符出现次数
      */
     private int keyAppearCount;
-    /**
-     * 空白行数
-     */
-    private int blackLines;
+
+    @Override
+    public String toString(){
+        return "统计文件：" + this.fileName
+                +"\r\n"+"代码行数：" + this.codeLines
+                +"\r\n"+"注释行数：" + this.noteLines
+                +"\r\n"+"空白行数：" + this.blackLines
+                +"\r\n"+"查找字符：" + this.key
+                +"\r\n"+"字符出现次数：" + this.keyAppearCount;
+    }
 }
