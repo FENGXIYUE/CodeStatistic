c85103be6db82b5f672991ead788bcff9ad94625
LIULONGTAO <nwpullt@163.com>
2021-07-20 09:56:17 +0800

    [20210720]-liulongtao 创建文件读取工具类和应用启动类

---
+++ src/main/java/APPStarter.java
@@ -0,0 +1,11 @@
+/**
+ * @author: LLT
+ * @description:
+ * @date: 2021/7/20 9:54 上午
+ * @modified By
+ */
+public class APPStarter {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/utils/FileReadUtil.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
+/**
+ * @author: LLT
+ * @description:
+ * @date: 2021/7/20 9:36 上午
+ * @modified By
+ */
+public class FileReadUtil {
+    public boolean checkUri(String uri) {
+        //判断传入的uri路径是否合法
+        if (StringUtils.isBlank(uri)) {
+            return false;
+        }
+        File file = new File(uri);
+        //判断文件是否存在以及对应的路径下是否为文件而不是文件夹
+        if (!file.exists() || !file.isFile()) {
+            return false;
+        }
+        return true;//返回文件大小
+    }
+}
