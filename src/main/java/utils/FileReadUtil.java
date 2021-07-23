ab5e4b863091c3a21c4dbe95eceda0fb8370e341
LIULONGTAO <nwpullt@163.com>
2021-07-23 18:01:37 +0800

    [20210723]-liulongtao 抽取新的工具类

--- src/main/java/utils/FileReadUtil.java
+++ src/main/java/utils/FileReadUtil.java
@@ -23,6 +23,7 @@ public class FileReadUtil {
      * @return
      */
     private static String JAVA = "java";
+
     public static boolean checkUri(String uri) {
         //判断传入的uri路径是否合法
         if (StringUtils.isBlank(uri)) {
@@ -34,7 +35,7 @@ public class FileReadUtil {
             return false;
         }
         //判断是否为java文件
-        if(!JAVA.equals(FileUtil.getType(file))){
+        if (!JAVA.equals(FileUtil.getType(file))) {
             return false;
         }
         //返回文件大小
@@ -60,11 +61,11 @@ public class FileReadUtil {
                 tempLine = in.readLine();
                 //统计总行数
                 result.setCodeLines(result.getCodeLines() + 1);
-                //统计字符出现次数
-                getCodeAppearCount(code, result, tempLine);
+                //统计字符出现次数z
+                FileContentStatisticUtil.getCodeAppearCount(code, result, tempLine);
                 if (StringUtils.isNotBlank(tempLine)) {
                     //统计注释行数
-                    multilineCommentFlag = getNotesLines(result, tempLine, multilineCommentFlag);
+                    multilineCommentFlag = FileContentStatisticUtil.getNotesLines(result, tempLine, multilineCommentFlag);
                 } else {
                     //统计空白行
                     result.setBlackLines(result.getBlackLines() + 1);
@@ -77,49 +78,5 @@ public class FileReadUtil {
         return result;
     }
 
-    /**
-     * 统计注释行数
-     *
-     * @param result
-     * @param tempLine
-     * @param multilineCommentFlag
-     * @return
-     */
-    private static boolean getNotesLines(CodeStatisticResponse result, String tempLine, boolean multilineCommentFlag) {
-        // 除去注释前的空格
-        tempLine = tempLine.trim();
-        //匹配单行注释 "//" 或 多行注释在一行
-        if (tempLine.startsWith("//") || (tempLine.startsWith("/*") && tempLine.endsWith("*/"))) {
-            result.setNoteLines(result.getNoteLines() + 1);
-            //匹配多行注释，注释内容在多行
-        } else if (tempLine.startsWith("/*") && !tempLine.endsWith("*/")) {
-            result.setNoteLines(result.getNoteLines() + 1);
-            multilineCommentFlag = true;
-            //匹配多行注释，每行注释加1
-        } else if (multilineCommentFlag) {
-            result.setNoteLines(result.getNoteLines() + 1);
-            //多行解释结尾
-            if (tempLine.endsWith("*/")) {
-                multilineCommentFlag = false;
-            }
-        }
-        return multilineCommentFlag;
-    }
-
-    /**
-     * 统计字符出现次数
-     *
-     * @param code
-     * @param result
-     * @param tempLine
-     */
-    private static void getCodeAppearCount(String code, CodeStatisticResponse result, String tempLine) {
-        if (tempLine.contains(code)) {
-            //字符出现次数加1
-            result.setKeyAppearCount(result.getKeyAppearCount() + 1);
-            //字符所在位置更新
-            result.getPositionRecord().add("java:" + result.getCodeLines());
-        }
-    }
 
 }
